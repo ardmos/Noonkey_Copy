96069ecca4b69c4ebf7e11ed1069891cb3abf767
ardmos <46891528+ardmos@users.noreply.github.com>
2021-10-28 14:04:07 +0900

    이미지, 사운드 추가했습니다.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 전체적인 흐름을 관장합니다. 
+/// 1. 가장 중요한 터치 인식.  빠른 터치인지 느린 터치인지 확인 후 캐릭터컨트롤러에서 알맞은 메서드 호출
+/// </summary>
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
