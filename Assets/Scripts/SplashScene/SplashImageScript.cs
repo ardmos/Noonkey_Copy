1c614b6a70a4e056fc74c6b9bfb5c82800e9e7df
ardmos <46891528+ardmos@users.noreply.github.com>
2021-10-28 15:39:11 +0900

    스플래시 씬 추가 완료.

---
+++ Assets/Scripts/SplashScene/SplashImageScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 스플래시 씬의 스플래시 이미지 애니메이션이 끝났을 때 호출되는 메서드가 있는 스크립트. 
+/// 하는 역할은 단순. SplashSceneManager의 씬 전환 메서드를 호출하는것. 
+/// 
+/// </summary>
+
+public class SplashImageScript : MonoBehaviour
+{
+    public SplashSceneManager sceneManager;
+    public void SplashImageAnimIsEnd()
+    {
+        sceneManager.LoadSampleScene();
+    }
+}
---
+++ Assets/Scripts/SplashScene/SplashSceneManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// 스플래시 씬을 관장하는 스크립트입니다. 
+/// 
+/// 1. 씬 시작시 스플래시 이미지 출력 애니메이션 실행.
+/// 2. 애니메이션이 끝나면 호출되는 애니메이션 이벤트로 샘플씬으로의 씬 전환 메서드 호출.  (애니메이션 이벤트 -> 해당 Image에 붙어있는 SplashImageScript -> 현 스크립트의 씬 전환 메서드)
+/// 
+/// </summary>
+
+public class SplashSceneManager : MonoBehaviour
+{
+
+    void Start()
+    {
+        // 1. 씬 시작시 스플래시 이미지 출력 애니메이션 실행.
+
+    }
+
+
+    // 2. 애니메이션이 끝나면 호출되는 애니메이션 이벤트로 샘플씬으로의 씬 전환 메서드 호출.  (애니메이션 이벤트 -> 해당 Image에 붙어있는 SplashImageScript -> 현 스크립트의 씬 전환 메서드)
+    public void LoadSampleScene()
+    {
+        try
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+        catch (System.Exception)
+        {
+
+            throw;
+        }        
+    }
+
+}
