ae7ec06c9b3cbde2b2788cdd19ad15bfd2c8bd5c
ardmos <46891528+ardmos@users.noreply.github.com>
2021-11-01 07:29:25 +0900

    테스트용 재화 늘리기 버튼 추가

---
+++ Assets/Scripts/SampleScene/테스트용/TestButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 테스트용 재화 늘리기 버튼 입니다.
+/// </summary>
+/// 
+public class TestButton : MonoBehaviour
+{
+    public void OnButtonClicked()
+    {
+        GameObject.Find("PlayerData").GetComponent<PlayerData>().heart += 500;
+    }
+}
